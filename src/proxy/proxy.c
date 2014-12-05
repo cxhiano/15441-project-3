6529f19b5c508d65224b07f11cffda1f67f402aa
jiangliu1993 <liuyimin10@gmail.com>
2014-12-05 15:58:52 -0500

    autolab test

--- src/proxy/proxy.c
+++ src/proxy/proxy.c
@@ -154,6 +154,7 @@ void handle_proxy_session(proxy_session *session)
                             bitrate = session->video->bitrates[i];
                         }
                     }
+                    fprintf(stdout, "%d\t%f\n", bitrate, session->video->throughput);
                     if (bitrate != 0) {
                         line = node->request_msg->head;
                         sprintf(modified_line, "%s%dSeg%s", node->path, bitrate,
